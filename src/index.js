daf1651c1b5b7d67cacc7dc97f07d708abaa7965
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-12 18:54:28 +0530

    add accountSlice and bonusSlice
    attach store to app using react-redux

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import accountReducer from "./slices/accountSlice";
+import bonusReducer from "./slices/bonusSlice";
+import { Provider } from "react-redux";
+
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    bonus: bonusReducer,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
