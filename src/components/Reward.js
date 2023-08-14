75fc4249bfa184d776a3aba2778aba010836ccfd
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-14 14:24:11 +0530

    add custom reducer for Reward component

--- src/components/Reward.js
+++ src/components/Reward.js
@@ -1,13 +1,17 @@
-// import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, incrementByAmount } from "../reducers/reward";
 
 export default function Reward() {
-  //   const dispatch = useDispatch();
-  //   const points = useSelector((state) => state.reward.points);
+  const dispatch = useDispatch();
+  const points = useSelector((state) => state.reward.points);
   return (
     <div>
       <h4>Reward Component</h4>
-      {/* <h3>Reward : {points}</h3> */}
-      {/* <button onClick={() => dispatch(increment())}>Increment +</button> */}
+      <h3>Reward : {points}</h3>
+      <button onClick={() => dispatch(increment())}>Increment +</button>
+      <button onClick={() => dispatch(incrementByAmount(10))}>
+        Increment By 10+
+      </button>
       <hr />
     </div>
   );
